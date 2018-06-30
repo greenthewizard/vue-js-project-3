fd7b2c31d532b98aa746d142d4d63d5ec03c9cf8
buddytherat <buddytherat@gmail.com>
2018-06-29 21:07:30 -0500

    Init

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!currentServer">Server Not Selected</p>
+        <p v-else>Server ID: {{ currentServer }}</p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: [
+            "currentServer"
+        ]
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="server in servers"
+                    :key="server.id"
+                    @click="selectServer(server)">
+                Server #{{ server.id }}: Status - {{ server.status }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            servers: [
+                { id: 1, status: 'Normal' },
+                { id: 2, status: 'Normal' },
+                { id: 3, status: 'Exquisite' },
+                { id: 4, status: 'On Fire' }
+            ]
+        }
+    },
+    methods: {
+        selectServer: function (server) {
+            this.$emit('updateCurrentServer', server);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
